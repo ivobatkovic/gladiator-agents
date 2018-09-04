ac66cd31c41611be9f0b9bbea744a680c7056737
amrit110 <amrit110@gmail.com>
2018-09-04 22:42:43 +0200

    Cleanup
    Move some functions out of main.py
    Q-learning works on simple cases, needs further investigation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 from q_learn import EPS_START, EPS_END, EPS_DECAY, QNetwork, ReplayMemory
-from q_learn import Transition, GAMMA
+from q_learn import Transition, GAMMA, create_batch_inputs, optimize_model
 
 # Global constants
 TARGET_UPDATE = 10
@@ -495,8 +495,8 @@ def add_text(screen, text, pos):
 
 def random_action():
     """Pick random action."""
-    return random.choice(range(len(ACTIONS)))
-    # return random.choice([3, 4, 5, 5])
+    # return random.choice(range(len(ACTIONS)))
+    return random.choice([3, 2, 4, 5, 5])
 
 
 def select_action(q_net, states, steps_done):
@@ -511,84 +511,6 @@ def select_action(q_net, states, steps_done):
     else:
         return torch.tensor([[random.randrange(len(ACTIONS))]],
                               dtype=torch.long), steps_done
-
-
-def create_batch_inputs(game, batch):
-    """Create batch of inputs to model."""
-    batch_size = len(batch.current_state)
-    state_batch = torch.zeros(batch_size, 16)
-    action_batch = torch.zeros(batch_size, 1)
-    reward_batch = torch.zeros(batch_size)
-    next_state_batch = torch.zeros(batch_size, 16)
-
-    # create state batch
-    for idx, states in enumerate(batch.current_state):
-        states_sample = []
-        rel_states = game.players[0].calc_rel_states(states)
-        for states in rel_states:
-            for state_pair in states:
-                if state_pair == None:
-                    state_inputs = torch.zeros(4)
-                else:
-                    state_inputs = torch.from_numpy(np.array(state_pair)).float()
-                states_sample.append(state_inputs)
-        states_sample = torch.cat(states_sample)
-        state_batch[idx] = states_sample
-
-    # create action batch
-    for idx, actions in enumerate(batch.action):
-        # player 1's action
-        action_batch[idx] = actions[0]
-
-    # create next state batch
-    for idx, states in enumerate(batch.next_state):
-        states_sample = []
-        rel_states = game.players[0].calc_rel_states(states)
-        for states in rel_states:
-            for state_pair in states:
-                if state_pair == None:
-                    state_inputs = torch.zeros(4)
-                else:
-                    state_inputs = torch.from_numpy(np.array(state_pair)).float()
-                states_sample.append(state_inputs)
-        states_sample = torch.cat(states_sample)
-        next_state_batch[idx] = states_sample
-
-    # create reward batch
-    for idx, scores in enumerate(batch.score):
-        # player 1's scores
-        reward_batch[idx] = scores[0]
-
-    return state_batch, action_batch, next_state_batch, reward_batch
-
-
-def optimize_model(game, memory, q_net, target_net, optimizer, batch_size):
-    """Perform one step of the optimization."""
-    if len(memory) < batch_size:
-        return
-    transitions = memory.sample(batch_size)
-    batch = Transition(*zip(*transitions))
-    state_batch, action_batch, next_state_batch, reward_batch = create_batch_inputs(game, batch)
-
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken
-    state_action_values = q_net(state_batch).gather(1, action_batch.long())
-
-    # Compute V(s_{t+1}) for all next states.
-    next_state_values = target_net(next_state_batch).max(1)[0].detach()
-
-    # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    # Compute Huber loss
-    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    # print('Loss: {}'.format(loss.item()))
-
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
 
 
 def plot_scores(pl_a_scores, pl_b_scores):
@@ -612,7 +534,7 @@ def run_random_game():
     gladiator_game = Game()
 
     # loop over episodes
-    n_episodes = 10000
+    n_episodes = 1000
 
     # --------main loop-----------
     for episode_idx in range(n_episodes):
@@ -650,7 +572,7 @@ def run_q_learning():
     target_net.eval()
 
     # batch size
-    batch_size = 32
+    batch_size = 128
 
     # optimizer
     optimizer = optim.RMSprop(q_net.parameters())
@@ -659,7 +581,7 @@ def run_q_learning():
     memory = ReplayMemory(10000)
 
     # loop over episodes
-    n_episodes = 1000
+    n_episodes = 50000
 
     # model updates
     steps_done = 0
--- q_learn.py
+++ q_learn.py
@@ -7,6 +7,8 @@ https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
 import random
 from collections import namedtuple
 
+import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -60,3 +62,81 @@ class QNetwork(nn.Module):
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
         return out
+
+
+def create_batch_inputs(game, batch):
+    """Create batch of inputs to model."""
+    batch_size = len(batch.current_state)
+    state_batch = torch.zeros(batch_size, 16)
+    action_batch = torch.zeros(batch_size, 1)
+    reward_batch = torch.zeros(batch_size)
+    next_state_batch = torch.zeros(batch_size, 16)
+
+    # create state batch
+    for idx, states in enumerate(batch.current_state):
+        states_sample = []
+        rel_states = game.players[0].calc_rel_states(states)
+        for states in rel_states:
+            for state_pair in states:
+                if state_pair == None:
+                    state_inputs = torch.zeros(4)
+                else:
+                    state_inputs = torch.from_numpy(np.array(state_pair)).float()
+                states_sample.append(state_inputs)
+        states_sample = torch.cat(states_sample)
+        state_batch[idx] = states_sample
+
+    # create action batch
+    for idx, actions in enumerate(batch.action):
+        # player 1's action
+        action_batch[idx] = actions[0]
+
+    # create next state batch
+    for idx, states in enumerate(batch.next_state):
+        states_sample = []
+        rel_states = game.players[0].calc_rel_states(states)
+        for states in rel_states:
+            for state_pair in states:
+                if state_pair == None:
+                    state_inputs = torch.zeros(4)
+                else:
+                    state_inputs = torch.from_numpy(np.array(state_pair)).float()
+                states_sample.append(state_inputs)
+        states_sample = torch.cat(states_sample)
+        next_state_batch[idx] = states_sample
+
+    # create reward batch
+    for idx, scores in enumerate(batch.score):
+        # player 1's scores
+        reward_batch[idx] = scores[0]
+
+    return state_batch, action_batch, next_state_batch, reward_batch
+
+
+def optimize_model(game, memory, q_net, target_net, optimizer, batch_size):
+    """Perform one step of the optimization for q-learning."""
+    if len(memory) < batch_size:
+        return
+    transitions = memory.sample(batch_size)
+    batch = Transition(*zip(*transitions))
+    state_batch, action_batch, next_state_batch, reward_batch = create_batch_inputs(game, batch)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken
+    state_action_values = q_net(state_batch).gather(1, action_batch.long())
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = target_net(next_state_batch).max(1)[0].detach()
+
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # print('Loss: {}'.format(loss.item()))
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
